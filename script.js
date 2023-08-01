500cdeab1f4ff4eb3562a63de05d62f45c88a3b8
EmirlanAmangeldiev2004 <emirlanamangeldiev37@gmail.com>
2023-08-01 11:05:40 +0600

    all change

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let container = document.querySelector(".container");
 let search = document.querySelector(".search")
 let btnSearch = document.querySelector(".btnSearch")
+let select1 = document.querySelector('.select-1')
+let select2 = document.querySelector('.select-2')
+let allData = null
 
 // let API = "https://restcountries.com/v3.1/all";
 
@@ -8,6 +11,7 @@ function getAPI(API){
    fetch(`https://restcountries.com/v3.1/${API}`)
   .then((data) => data.json())
   .then((unp) => {
+   allData = unp
    render(unp)
   });
 }
@@ -17,7 +21,6 @@ getAPI('all')
 function render(data){
    container.innerHTML = ""
    data.forEach((el)=>{
-      console.log(el);
       container.innerHTML += `
          <div class = 'block'>
             <img class = 'img' src="${el.flags.png}" alt="flag">
@@ -33,6 +36,59 @@ function render(data){
 
 btnSearch.addEventListener('click',()=>{
    getAPI(`name/${search.value}`)
+})
+
+select1.addEventListener('change', (e)=>{
+   let values = e.target.value
+   if(values === 'all'){
+      render(allData)
+   }
+   if(values === 'europe'){
+      let res = allData.filter((el)=>{
+         return el.region === 'Europe'
+      })
+      render(res)
+   }
+   if(values === 'asia'){
+      let res = allData.filter((el)=>{
+         return el.region === 'Asia'
+      })
+      render(res)
+   }
+   if(values === 'africa'){
+      let res = allData.filter((el)=>{
+         return el.region === 'Africa'
+      })
+      render(res)
+   }
+   if(values === 'americas'){
+      let res = allData.filter((el)=>{
+         return el.region === 'Americas'
+      })
+      render(res)
+   }
+})
+
+select2.addEventListener('change',(e)=>{
+   let values = e.target.value
+   if(values === 'a-z'){
+      let res = allData.sort((a,b)=>{
+         return a.name.common.localeCompare(b.name.common)
+      })
+      render(res)
+   }
+   if(values === 'z-a'){
+      let res = allData.sort((a,b)=>{
+         return b.name.common.localeCompare(a.name.common)
+      })
+      render(res)
+   }
+   if(values === 'population'){
+      let res = allData.sort((a,b)=>{
+         return b.population - a.population
+      })
+      render(res)
+   }
 })
 
 // search.addEventListener('input',(e)=>{
